d21dd5df8450bd27d41cfe89814ac52c2f5e0cab
Kanno Beck <drkannobeck@gmail.com>
2023-03-28 21:15:48 +0100

    update-header-mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,10 +7,12 @@ const Header = () => {
   return (
     <div className="header--container">
       <div className="header--sub_container">
-        <h1>Power your future with renewable energy.</h1>
-        <p>Reduce your carbon footprint. Save money. Go Kibobo.</p>
+        <div className="header--left_container">
+          <h1>Power your future with renewable energy.</h1>
+          <p>Reduce your carbon footprint. Save money. Go Kibobo.</p>
 
-        <button type="button" className="main__button"><a href="#">Contact us</a></button>
+          <button type="button" className="main__button"><a href="#">Contact us</a></button>
+        </div>
       </div>
 
       <img src={solar} alt="solar panel"/>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,30 +13,33 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState (false);
 
   return (
-    <div className="poa__navbar">
-      <div className="poa__navbar-links">
-        <div className="poa__navbar-links_logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="poa__navbar-links_container">
-          <Menu />
-        </div>
+    <div className="route">
+      <div className="poa__navbar">
 
-      </div>
-
-      <div className="poa__navbar-menu">
-        {toggleMenu
-          ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false)}/>
-          : <RiMenu3Line color="#000" size={27} onClick={() => setToggleMenu(true)}/>
-        }
-        {toggleMenu && (
-          <div className="poa__navbar-menu_container scale-up-center">
-            <div className="poa__navbar-menu_container-links">
-              <Menu />
-            </div>
+        <div className="poa__navbar-links">
+          <div className="poa__navbar-links_logo">
+            <img src={logo} alt="logo" />
           </div>
-        )}
+          <div className="poa__navbar-links_container">
+            <Menu />
+          </div>
+
+        </div>
+        <div className="poa__navbar-menu">
+          {toggleMenu
+            ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false)}/>
+            : <RiMenu3Line color="#000" size={27} onClick={() => setToggleMenu(true)}/>
+          }
+          {toggleMenu && (
+            <div className="poa__navbar-menu_container">
+              <div className="poa__navbar-menu_container-links">
+                <Menu />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
+
     </div>
   )
 }
