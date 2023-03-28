80d59b80d5de97bf94fc7a312d244e10bbd40384
Kanno Beck <drkannobeck@gmail.com>
2023-03-28 19:00:12 +0100

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './header.css';
 
+import solar from '../../assets/solar-panel-img.png';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header--container">
+      <div className="header--sub_container">
+        <h1>Power your future with renewable energy.</h1>
+        <p>Reduce your carbon footprint. Save money. Go Kibobo.</p>
+
+        <button type="button" className="main__button"><a href="#">Contact us</a></button>
+      </div>
+
+      <img src={solar} alt="solar panel"/>
+    </div>
   )
 }
 
