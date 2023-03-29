a6a92e11fc4f16f6336c300683941a2830c0aca5
Kanno Beck <drkannobeck@gmail.com>
2023-03-29 07:37:34 +0100

    update-services

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -11,7 +11,6 @@ const Product = () => {
         <img className="prd-img" src={prd} alt="green inverter"/>
       </div>
 
-
       <div className="prd--sub">
         <p className="product--sub_container">At Kibobo, we work collaboratively with home owners, listening carefully to needs and preferences, then bringing our solar and engineering to the table - the results are tailored installations that suit the context, tick the boxes, and meet the budget.</p>
         <button type="button" className="main__button"><a href="#">Learn more <img src={icn} alt="arrow"/></a></button>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,34 @@ import './services.css';
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="services__container">
+      <div className="services__sub-container">
+        <div className="services--header">
+          <h2>Our <br /> Services</h2>
+          <p>Integrated hardware and simple design achieve this by securing the panels close to your roof and to each other for a minimalist aesthetic.</p>
+        </div>
+
+        <div className="services--sub_header">
+          <div className="module">
+            <h2>Solar Accessories</h2>
+            <p>Integrated hardware and simple design achieve this by securing the panels close to your roof and to each other for a minimalist aesthetic.</p>
+          </div>
+
+          <div className="module">
+            <h2>Solar Accessories</h2>
+            <p>Integrated hardware and simple design achieve this by securing the panels close to your roof and to each other for a minimalist aesthetic.</p>
+          </div>
+
+          <div className="module">
+            <h2>Solar Accessories</h2>
+            <p>Integrated hardware and simple design achieve this by securing the panels close to your roof and to each other for a minimalist aesthetic.</p>
+          </div>
+        </div>
+
+        <button type="button" className="main__button contact"><a href="#">Contact us</a></button>
+
+      </div>
+    </div>
   )
 }
 
