5dc5cef1879240a1ce8c54759ee5f23820ea194a
Kanno Beck <drkannobeck@gmail.com>
2023-04-10 15:46:06 +0100

    update-header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import './header.css';
 
-import solar from '../../assets/solar-panel-img.png';
+import sol from '../../assets/sol.png';
+
 
 const Header = () => {
   return (
-    <div id="home" className="header--container">
-      <div className="header--sub_container">
-        <div className="header--left_container">
-          <h1>Power your future with renewable energy.</h1>
-          <p>Reduce your carbon footprint. Save money. Go Kibobo.</p>
+    <><div id="home" className="header--container">
+      <div className="header--module">
 
-          <button type="button" className="main__button"><a href="#">Contact us</a></button>
+        <div className="header--sub_container">
+          <div className="header--left_container">
+            <h1>Power your future with renewable energy.</h1>
+            <p>Reduce your carbon footprint. Save money. Go Kibobo.</p>
+
+            <div className="mob__img">
+              <img src={sol} alt="workers installing a solar panel" />
+            </div>
+
+          </div>
+
+
+          <div className="header__button">
+            <button type="button" className="main__button"><a href="#">Contact us</a></button>
+          </div>
+
+        </div>
+        <div className="header__img">
+          <img src={sol} alt="workers installing a solar panel" />
         </div>
       </div>
 
-      <img src={solar} alt="solar panel"/>
-    </div>
+    </div><p className="footer-note">Clean energy management for your home.</p></>
   )
 }
 
